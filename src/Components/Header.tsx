438d4eb7902931c22229fd16489bbd930a59dc8c
aasn0119 <abhitrivedi119@gmail.com>
2024-05-24 00:13:42 +0530

    feat: Add Firebase integration for backend services

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+const logo = require("../Assets/logo.png");
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="flex flex-wrap z-10 sm:flex-row gap-5 items-center justify-between drop-shadow-md bg-gradient-to-r from-myBlue to-myPink px-5 py-5 md:py-2 text-white">
+      <img className="w-[70px] drop-shadow-md cursor-pointer" src={logo} alt="logo" />
+    </div>
+  );
 };
 
 export default Header;
--- src/Pages/Layout.tsx
+++ src/Pages/Layout.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router";
+import Header from "../Components/Header";
 
 type Props = {};
 
 function Layout({}: Props) {
   return (
     <div className="h-[100vh] flex flex-col">
-      {/* <Header /> */}
-      Header
+      <Header />
       <div className="bg-pattern flex-1 max-h-[90%] overflow-y-scroll">
         <Outlet />
       </div>
